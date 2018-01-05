032efba590e0651caff2e9ff0cb2b7adf43bc4a6
jake100 <reigndown@live.ca>
2018-01-04 17:56:36 -0800

    changed style

---
+++ chat/index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
+      socket.on('makeGreen', function(isGreen){
+		io.emit('makeGreen', isGreen);
+	});
+});
+
+
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
