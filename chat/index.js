30b678bcb2af3a0eecbbe20f326f829a2da61e85
jake100 <reigndown@live.ca>
2018-01-07 23:32:53 -0800

    fixed look of site

--- chat/index.js
+++ chat/index.js
@@ -2,7 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
